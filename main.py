5d0b3b263a60e8b72b359d34d0f2526f8e48fb3b
Dmitriy Kurzykin <ametek84@mail.ru>
2022-08-28 22:38:56 +0300

    Task complete

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ superhero = ['Hulk', 'Captain America', 'Thanos']
 url = 'https://akabab.github.io/superhero-api/api/'
 superhero_info = {}
 all_superhero = requests.get(url + '/all.json').json()
-from pprint import pprint
-pprint(all_superhero)
+# from pprint import pprint
+# pprint(all_superhero)
+for superhero_list in all_superhero:
+    if superhero_list['name'] in superhero:
+        superhero_info[superhero_list['name']] = superhero_list['powerstats']['intelligence']
+smartest_hero = max(superhero_info, key=superhero_info.get)
+print(f'Самый умный: {smartest_hero}')
